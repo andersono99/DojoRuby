9c30250f7b7b5c42f68aff7b159b5211baf0f3e7
Cesar Muñoz <cesar.munoz@ceiba.com.co>
2018-09-08 10:04:11 -0500

    first commit

---
+++ app/controllers/applies_controller.rb
@@ -0,0 +1,16 @@
+class AppliesController < ApplicationController
+    def index
+        @applies = Apply.all 
+    end
+    def show
+        @applies = Apply.find(params[:id])
+    end
+    def new
+        @applies = Apply.new
+    end
+    def create 
+        @applies = Apply.new(title: params[:apply][:title], body: params[:apply][:body], name: params[:apply][:name], edad: params[:apply][:edad])
+        @applies.save
+        redirec_to @applies
+    end
+end
